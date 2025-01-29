07ce3140cbe64e89c29e71c2eb647f4c108e3a0c
Karthik-SS <karthieus@gmail.com>
2025-01-29 11:18:51 -0800

    Modified About Section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
             <h1 className="text-3xl font-bold">About Us</h1>
             <p className="mt-4 text-gray-700">
                 At Humming Bird's Consulting, We Specialize In Empowering Startups By Providing Engineering Expertise,
-                Strategic Insights, Enabling Data-Driven Decision Making To Achieve Your Business Goals.
+                Strategic Insights, Enabling Data-Driven Decision Making To Achieve Their Business Goals.
             </p>
         </div>
     );
