a46de9e1f4988ef3abc5f0fcdc7cfde52410a519
Karthik-SS <karthieus@gmail.com>
2025-01-28 19:34:05 -0800

    Modified Some Text In About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <div className="max-w-4xl mx-auto py-20">
             <h1 className="text-3xl font-bold">About Us</h1>
             <p className="mt-4 text-gray-700">
-                At Humming Bird Consulting, We Specialize In Empowering Startups By Providing Engineering Expertise,
+                At Humming Bird's Consulting, We Specialize In Empowering Startups By Providing Engineering Expertise,
                 Strategic Insights, Enabling Data-Driven Decision Making To Achieve Your Business Goals.
             </p>
         </div>
